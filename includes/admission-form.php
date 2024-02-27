1a6dd5a546bbac6a7100c8abf04e11f249231328
Hiroshi Ozeki <ozeki.hiroshi@gmail.com>
2024-02-27 14:36:03 +0200

    update form

--- includes/admission-form.php
+++ includes/admission-form.php
@@ -1,39 +1,25 @@
-<div class="admission-form">
-    <h2>Admission Form</h2>
-    <form action="" method="post">
-        <!-- Admission Form Fields -->
-        <label for="sname">Last Name:</label>
-        <input type="text" name="sname" id="sname" required>
-        <br>
-        <label for="gname">First Name:</label>
-        <input type="text" name="gname" id="gname" required>
-        <br>
-        <label for="contact">Contact:</label>
-        <input type="text" name="contact" id="contact" required>
-        <br>
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required>
-        <br>
-        <label for="address">Address:</label>
-        <input type="text" name="address" id="address" required>
-        <br>
-        <label for="class">Class:</label>
-        <input type="text" name="class" id="class" required>
-        <br>
-        <label for="shift">Shift:</label>
-        <input type="text" name="shift" id="shift" required>
-        <br>
-        <label for="gender">Gender:</label>
-        <input type="text" name="gender" id="gender" required>
-        <br>
-        <label for="blgroup">Blood Group:</label>
-        <input type="text" name="blgroup" id="blgroup" required>
-        <br>
-        <label for="division">Division:</label>
-        <input type="text" name="division" id="division" required>
-        <br>
-        <input type="submit" value="Submit">
-    </form>
-</div>
+<?php
+// フォームが送信された場合
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // フォームデータを受け取る
+    $sname = sanitize_text_field($_POST["sname"]);
+    $gname = sanitize_text_field($_POST["gname"]);
+    $contact = sanitize_text_field($_POST["contact"]);
+    $email = sanitize_email($_POST["email"]);
+    $address = sanitize_text_field($_POST["address"]);
+    $class = sanitize_text_field($_POST["class"]);
+    $shift = intval($_POST["shift"]);
+    $gender = sanitize_text_field($_POST["gender"]);
+    $blgroup = sanitize_text_field($_POST["blgroup"]);
+    $division = intval($_POST["division"]);
+    $age = intval($_POST["age"]);
+
+    // 確認画面を表示する
+    include 'confirmation-page.php';
+} else {
+    // フォームを表示する
+    include 'registration-form.php';
+}
+?>
 
 
