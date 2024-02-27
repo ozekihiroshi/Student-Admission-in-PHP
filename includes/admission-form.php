2d14882029c9b1c0bb0d9b1160a504810ed89e0a
Hiroshi Ozeki <ozeki.hiroshi@gmail.com>
2024-02-27 14:00:25 +0200

    update form

--- includes/admission-form.php
+++ includes/admission-form.php
@@ -1,39 +1,38 @@
 <div class="admission-form">
-    <h2>入学フォーム</h2>
+    <h2>Admission Form</h2>
     <form action="" method="post">
-        <!-- 入学フォームのフィールド -->
-<label for="sname">姓：</label>
-<input type="text" name="sname" id="sname" required>
-<br>
-<label for="gname">名：</label>
-<input type="text" name="gname" id="gname" required>
-<br>
-<label for="contact">連絡先：</label>
-<input type="text" name="contact" id="contact" required>
-<br>
-<label for="email">メールアドレス：</label>
-<input type="email" name="email" id="email" required>
-<br>
-<label for="address">住所：</label>
-<input type="text" name="address" id="address" required>
-<br>
-<label for="class">クラス：</label>
-<input type="text" name="class" id="class" required>
-<br>
-<label for="shift">シフト：</label>
-<input type="text" name="shift" id="shift" required>
-<br>
-<label for="gender">性別：</label>
-<input type="text" name="gender" id="gender" required>
-<br>
-<label for="blgroup">血液型：</label>
-<input type="text" name="blgroup" id="blgroup" required>
-<br>
-<label for="division">部門：</label>
-<input type="text" name="division" id="division" required>
-<br>
-<input type="submit" value="送信">
-       
+        <!-- Admission Form Fields -->
+        <label for="sname">Last Name:</label>
+        <input type="text" name="sname" id="sname" required>
+        <br>
+        <label for="gname">First Name:</label>
+        <input type="text" name="gname" id="gname" required>
+        <br>
+        <label for="contact">Contact:</label>
+        <input type="text" name="contact" id="contact" required>
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required>
+        <br>
+        <label for="address">Address:</label>
+        <input type="text" name="address" id="address" required>
+        <br>
+        <label for="class">Class:</label>
+        <input type="text" name="class" id="class" required>
+        <br>
+        <label for="shift">Shift:</label>
+        <input type="text" name="shift" id="shift" required>
+        <br>
+        <label for="gender">Gender:</label>
+        <input type="text" name="gender" id="gender" required>
+        <br>
+        <label for="blgroup">Blood Group:</label>
+        <input type="text" name="blgroup" id="blgroup" required>
+        <br>
+        <label for="division">Division:</label>
+        <input type="text" name="division" id="division" required>
+        <br>
+        <input type="submit" value="Submit">
     </form>
 </div>
 
